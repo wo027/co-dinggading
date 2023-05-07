45e1cd170562488bf81a81c9cab39aa6d7c1cca0
SeyoungLee <79364449+seyoonge@users.noreply.github.com>
2023-05-08 08:45:32 +0900

    Update 1934.sql

--- seyoung/1934.sql
+++ seyoung/1934.sql
@@ -1,5 +1,5 @@
 SELECT s.user_id, 
-ROUND(IFNULL((COUNT(CASE WHEN action = 'confirmed' THEN c.time_stamp END) / COUNT(c.user_id)), 0),2confirmation_rate 
+ROUND(IFNULL((COUNT(CASE WHEN action = 'confirmed' THEN c.time_stamp END) / COUNT(c.user_id)), 0),2) confirmation_rate 
 FROM Signups s
 LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
