c36a11d5dbb86f3d6f88d10070b4cacacb63b545
7ryean <yoovely1212@naver.com>
2023-05-08 23:47:30 +0900

    불필요한 DISTINCT 삭제

--- gayeon/1581.sql
+++ gayeon/1581.sql
@@ -1,6 +1,6 @@
 SELECT
     customer_id
-  , COUNT(DISTINCT Visits.visit_id) AS count_no_trans
+  , COUNT(Visits.visit_id) AS count_no_trans
 FROM Visits
     LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
 WHERE Transactions.transaction_id IS NULL
