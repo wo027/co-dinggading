28bbcb722cf1ee1a2845c2433fadef1bb1058379
SeyoungLee <79364449+seyoonge@users.noreply.github.com>
2023-05-19 17:18:53 +0900

    Create 176.sql

---
+++ seyoung/176.sql
@@ -0,0 +1,10 @@
+SELECT 
+    MAX(salary) AS SecondHighestSalary
+FROM 
+    Employee
+WHERE
+    salary < (  SELECT
+                    MAX(salary)
+                FROM 
+                    Employee )
+;
