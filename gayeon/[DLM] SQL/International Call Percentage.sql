f36fb0ee10f65c8066c20132bf2de8ac381a1253
7ryean <yoovely1212@naver.com>
2023-07-09 19:14:47 +0900

    CTE 제거(다중 INNER JOIN 사용)

--- gayeon/[DLM] SQL/International Call Percentage.sql
+++ gayeon/[DLM] SQL/International Call Percentage.sql
@@ -1,23 +1,33 @@
-WITH phone_calls_info AS (
-    SELECT 
-        phone_calls.caller_id
-      , phone_info.country_id
-      , phone_calls.receiver_id
-    FROM
-        phone_calls
-        INNER JOIN phone_info
-          ON phone_calls.caller_id = phone_info.caller_id
-)
-
+-- SUM 사용
+-- COUNT에 비해 성능이 더 좋은가?
 SELECT 
     ROUND(
         SUM(
-            CASE WHEN phone_calls_info.country_id != phone_info.country_id
+            CASE WHEN caller_phone_info.country_id != reciever_phone_info.country_id
             THEN 1 END
-        )::DECIMAL / COUNT(phone_calls_info.caller_id) * 100, 1
+        )::DECIMAL / SUM(1) * 100, 1
     ) AS international_calls_pct
 FROM 
-    phone_calls_info
-    INNER JOIN phone_info
-      ON phone_calls_info.receiver_id = phone_info.caller_id
+    phone_calls
+    INNER JOIN phone_info AS caller_phone_info
+      ON phone_calls.caller_id = caller_phone_info.caller_id
+    INNER JOIN phone_info AS reciever_phone_info
+      ON phone_calls.receiver_id = reciever_phone_info.caller_id
+;
+
+-- COUNT 사용
+-- SUM과 성능 차이가 있는가? 성능 차이가 없다면 COUNT를 사용하는게 쿼리 로직 자체를 이해하기에는 더 간편한 느낌..? SUM()은 뭔가 정제되어 있는 느낌
+SELECT 
+    ROUND(
+        COUNT(
+            CASE WHEN caller_phone_info.country_id != reciever_phone_info.country_id
+            THEN phone_calls.caller_id END
+        )::DECIMAL / COUNT(phone_calls.caller_id) * 100, 1
+    ) AS international_calls_pct
+FROM 
+    phone_calls
+    INNER JOIN phone_info AS caller_phone_info
+      ON phone_calls.caller_id = caller_phone_info.caller_id
+    INNER JOIN phone_info AS reciever_phone_info
+      ON phone_calls.receiver_id = reciever_phone_info.caller_id
 ;
