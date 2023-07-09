920da1ba76e078f39d772acab695d36830d802be
SeyoungLee <79364449+seyoonge@users.noreply.github.com>
2023-07-09 19:34:30 +0900

    Delete 02_Supercloud Customer.sql

--- seyoung/[DLM] SQL_Week_10/02_Supercloud Customer.sql
+++
@@ -1,9 +0,0 @@
-SELECT c.customer_id
-FROM customer_contracts c
-  INNER JOIN products p
-  ON c.product_id = p.product_id
-GROUP BY 1
-HAVING COUNT(DISTINCT p.product_category) = (
-    SELECT COUNT(DISTINCT product_category)
-    FROM products)
-;
